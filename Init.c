0cb12c23d2bf020e2564fe4f852b04db613088a5
Yogesh Choudhary <yogeshchoudhary@Yogeshs-MacBook-Pro.local>
2017-10-18 01:21:46 +0530

    Function Declarations

--- Init.c
+++ Init.c
@@ -4,12 +4,15 @@
 #define queueSize 500
 int maxResources[numResources];
 
+unsigned long long int timeElapsed;
+
 struct process
 {
 	int burst;
 	int Res[NumResources];
 };
 /* Ready Queue Starts*/
+
 struct queue
 {
 	int front,rear;
@@ -32,6 +35,7 @@ struct process pop(struct queue *q)
 {
 	return q.Arr[q.front++];
 }
+
 /* Ready Queue Ends*/
 void initMaxResources()
 {
@@ -41,7 +45,36 @@ void initMaxResources()
 
 void initAll()
 {
+	timeElapsed=0;
 	initMaxResources();
+}
+
+unsigned long long int getArrivalTime()
+{
+	return timeElapsed;
+}
+
+void newProcess()
+{
+	printf("Enter new process details : ");
+	struct process p;
+	p=(struct process *)malloc(sizeof(struct process));
+	printf("Enter burst Time")
+}
+
+void requestResources(struct process processNumber)
+{
+
+}
+
+void releaseResources(struct process processNumber)
+{
+
+}
+
+void abortProcess(struct process processNumber)
+{
+
 }
 
 int main()

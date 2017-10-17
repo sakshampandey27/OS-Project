d7f62b026cea6f5d296f88baa7c727d73cf97ab6
Yogesh Choudhary <yogeshchoudhary@Yogeshs-MacBook-Pro.local>
2017-10-18 01:13:10 +0530

    Queue structure added

--- Init.c
+++ Init.c
@@ -1,19 +1,38 @@
 #include<stdio.h>
 #include<stdlib.h>
 #define numResources 5
+#define queueSize 500
 int maxResources[numResources];
 
 struct process
 {
 	int burst;
 	int Res[NumResources];
-}
-
+};
+/* Ready Queue Starts*/
 struct queue
 {
-	
+	int front,rear;
+	int Arr[queueSize];
+};
+
+int isEmpty(struct queue *q)
+{
+	if(q.front==q.rear)
+		return 1;
+	return 0;
 }
 
+void push(struct queue *q,struct process p)
+{
+	q.Arr[q.rear++]=p;
+}
+
+struct process pop(struct queue *q)
+{
+	return q.Arr[q.front++];
+}
+/* Ready Queue Ends*/
 void initMaxResources()
 {
 	for(int i=0;i<numResources;i++)
